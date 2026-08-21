1ebdc07edf431875a84168a3ca078af392b1f6ca
Shivam Kumar Pandey <shivampandeyy14@gmail.com>
2026-08-21 12:14:40 +0530

    hello world

---
+++ hello.java
@@ -0,0 +1,10 @@
+class hello{
+    public static void main(String[]args){
+        System.out.println("hello world");
+    }
+}
+class test{
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
